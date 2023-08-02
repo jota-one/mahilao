282b4cbfc28094f0c065cbe7f5aa37414a154326
Joel Poulin <joel@jota.one>
2023-08-02 07:50:02 +0200

    feat: mahilao page + changes in women empowerment page

--- components/content/BulletBlock.vue
+++ components/content/BulletBlock.vue
@@ -4,7 +4,7 @@ type Props = {
   sepClass?: string
 }
 withDefaults(defineProps<Props>(), {
-  titleClass: '',
+  titleClass: 'font-subtitle text-xl mb-4',
   sepClass: ''
 })
 const slots = useSlots()
@@ -13,7 +13,7 @@ const slots = useSlots()
 <template>
   <div class="mb-4 sm:mb-10 text-md">
     <hr class="w-7 border-2 mb-2" :class="sepClass" />
-    <h3 v-if="slots.title" class="font-subtitle text-xl mb-4" :class="titleClass"><slot name="title" /></h3>
+    <h3 v-if="slots.title" :class="titleClass"><slot name="title" /></h3>
     <slot />
   </div>
 </template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,11 @@ export default <Partial<Config>>{
           500: '#E85F3F',
           DEFAULT: '#E85F3F',
         },
+        mahilao: {
+          200: '#b66c7a',
+          500: '#c51b3b',
+          DEFAULT: '#c51b3b',
+        },
         farming: {
           200: '#9CA59A',
           500: '#4D7F41',
